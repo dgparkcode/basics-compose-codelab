4ef5e741d9c9d5a59a722932d2a21746453a92fd
dgparkcode <dgpark.code@gmail.com>
2023-01-05 23:20:18 +0900

    Update for step 12

--- app/src/main/java/com/dgparkcode/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/dgparkcode/basicscodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dgparkcode.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -27,6 +28,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dgparkcode.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -103,7 +105,11 @@ private fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 onClick = { expanded = !expanded }
@@ -117,6 +123,19 @@ private fun Greeting(name: String) {
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 private fun GreetingsPreview() {
+    BasicsCodelabTheme {
+        Greetings()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Composable
+private fun GreetingsPreviewDark() {
     BasicsCodelabTheme {
         Greetings()
     }
